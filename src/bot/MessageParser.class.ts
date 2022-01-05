550d87e12c5b50e0e819070a95aa56ebd80e0e87
Saveliy <59727464+Mrcavas@users.noreply.github.com>
2022-01-05 16:53:36 +0700

    Change shitcode to regex

--- src/bot/MessageParser.class.ts
+++ src/bot/MessageParser.class.ts
@@ -3,36 +3,15 @@ import { injectable } from 'inversify'
 import 'reflect-metadata'
 import { MessageParser as IMessageParser } from '../abstracts/interfaces/MessageParser.interface'
 import { Codeblock } from './Codeblock.class'
-import { ProgrammingLanguage, ProgrammingLanguages } from '../abstracts/types/Codeblock.types'
+import {
+  ProgrammingLanguage,
+  ProgrammingLanguages
+} from '../abstracts/types/Codeblock.types'
 
 @injectable()
 export class MessageParser implements IMessageParser {
   private static findCodeblocks(s: string): string[] {
-    const lines = s.split('\n')
-    let codeblocks: string[] = []
-    let codeblock = ''
-    let isCodeblock = false
-    for (const line of lines) {
-      if (isCodeblock) {
-        if (line.match(/^```\w*/) !== null) {
-          codeblock += line
-          isCodeblock = false
-          codeblocks.push(codeblock)
-          codeblock = ''
-        } else {
-          codeblock += line + '\n'
-        }
-      } else {
-        if (line.match(/^```\w*/) !== null) {
-          codeblock += line + '\n'
-          isCodeblock = true
-        }
-      }
-    }
-    return codeblocks.filter(v =>
-      v.split('\n')[0].match(/^```\w*/) !== null &&
-      v.split('\n')[v.split('\n').length - 1].match(/^```\w*/) !== null
-    )
+    return s.match(/(?<!\\)(\`\`\`)(\w+\n)([^]*?)(?<!\\)(\`\`\`)/g) || []
   }
 
   parse(message: string): RuntimeBotQuery {
@@ -45,18 +24,18 @@ export class MessageParser implements IMessageParser {
         } else {
           return
         }
-      }).filter(v => v) as Codeblock[]
+      })
+      .filter(v => v) as Codeblock[]
     return new RuntimeBotQuery(parsedCodeblocks)
   }
 
   parseCodeblock(codeblock: string): {
-    lang: string,
+    lang: string
     code: string
   } {
-    const lines = codeblock.split('\n')
-    const lang = lines[0].replaceAll('```', '')
-    const code = lines.slice(1, lines.length - 1).join('\n')
-    return { lang, code }
+    const res = codeblock.match(
+      /(?<!\\)(\`\`\`)(\w+\n)([^]*?)(?<!\\)(\`\`\`)/
+    )
+    return { lang: (res || [])[2], code: (res || [])[3] }
   }
-
 }
